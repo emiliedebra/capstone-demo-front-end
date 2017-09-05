956ecded527ba069c4bba363de7300c41be1b9bb
Emilie Wood <emilie@anotherway.co.za>
2017-09-05 20:09:03 +0200

    Configure home page for back-end data

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -4,14 +4,14 @@
   <v-card flat class="home">
     
     <!-- Year Title: Dummy -->
-    <v-card v-for="year in reports" :key="year.year">
-      <v-card-title>
+    <!-- <v-card v-for="year in posts.outputs" :key="year.outputs"> -->
+      <!-- <v-card-title>
         <h4 class="ma-0 pa-0">{{ year.year }}</h4>
       </v-card-title>
-      <v-divider></v-divider>
+      <v-divider></v-divider> -->
 
       <!-- Report Cards -->
-      <v-card v-for="item in year.data" :key="item.title">
+      <v-card v-for="item in posts" :key="item.title">
         <v-card-title class="pb-0 mb-0">
           <div>
             <div class="headline">
@@ -21,12 +21,12 @@
             </div>
             <p class="pa-0 ma-0">{{ item.author }}</p>
 
-            <p class="pa-0 ma-0 grey--text">{{ item.coAuthor }}</p>
+            <p class="pa-0 ma-0 grey--text">{{ item.type }}</p>
           </div>
         </v-card-title>
 
         <!-- Abstract Text - Dummy Function -->
-        <v-card-text v-show="item.show">{{ item.abstract }}</v-card-text>
+        <v-card-text v-if="item.show">{{ item.additional_info }}</v-card-text>
 
         <!-- Button Panel -->
         <v-btn class="mr-0 pa-0" small flat @click.native="item.show = !item.show"> Abstract </v-btn>
@@ -35,7 +35,7 @@
         <v-divider></v-divider>
       </v-card>
     </v-card> 
-  </v-card>
+  <!-- </v-card> -->
 </template>
 
 <script>
@@ -44,76 +44,35 @@ import axios from 'axios';
 export default {
   name: 'home',
   data() {
-    // return {
-    //   posts: [],
-    //   errors: [],
-    // };
     return {
-      // NB: temporary data for dev purposes
-      reports: [
-        {
-          year: 2017,
-          data: [{
-            title: 'Paper One',
-            author: 'Emilie Wood',
-            coAuthor: 'Tommie Meyer',
-            abstract: `I'm a thing. But, like most politicians, he promised more than he could deliver. 
-            You won't have time for sleeping, soldier, not with all the bed making you'll be doing. 
-            Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! 
-            You're going to do his laundry? I've got to find a way to escape.`,
-            show: false,
-          },
-          { title: 'Paper Two',
-            author: 'Emilie Wood',
-            coAuthor: 'Tommie Meyer',
-            abstract: 'No abstract to show.',
-            show: false,
-          },
-          {
-            title: 'Paper Three',
-            author: 'Emilie Wood',
-            coAuthor: 'Tommie Meyer',
-            abstract: 'No abstract to show.',
-            show: false,
-          },
-          ],
-        },
-
-        {
-          year: 2016,
-          data: [{
-            title: 'Paper One',
-            author: 'Emilie Wood',
-            coAuthor: 'Tommie Meyer',
-            abstract: `I'm a thing. But, like most politicians, he promised more than he could deliver. 
-            You won't have time for sleeping, soldier, not with all the bed making you'll be doing. 
-            Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! 
-            You're going to do his laundry? I've got to find a way to escape.`,
-            show: false,
-          },
-          {
-            title: 'Paper Two',
-            author: 'Emilie Wood',
-            coAuthor: 'Tommie Meyer',
-            abstract: 'No abstract to show.',
-            show: false,
-          },
-          {
-            title: 'Paper Three',
-            author: 'Emilie Wood',
-            coAuthor: 'Tommie Meyer',
-            abstract: 'No abstract to show.',
-            show: false,
-          },
-          ],
-        },
-      ],
+      posts: [],
+      errors: [],
     };
+    // return {
+    //   // NB: temporary data for dev purposes
+    //   reports: [
+    //     {
+    //       year: 2017,
+    //       data: [{
+    //         title: 'Paper One',
+    //         author: 'Emilie Wood',
+    //         coAuthor: 'Tommie Meyer',
+    //         abstract: `I'm a thing. But, like most politicians, he promise|d more than he could deliver. 
+    //         You won't have time for sleeping, soldier, not with all the bed making you'll be doing. 
+    //         Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! 
+    //         You're going to do his laundry? I've got to find a way to escape.`,
+    //         show: false,
+    //       },
+    //       ],
+    //     },
+    //   },
+    // };
   },
   created() {
-    axios.get('http://localhost:3000/')
+    axios
+      .get('http://localhost:3000')
       .then((response) => {
-        this.posts = response.data;
+        this.posts = response.data.outputs.map(o => Object.assign(o, { show: false }));
       });
   },
 };
